ce68afa8a8e1aacbeacca632c9dfd9a1c19a36b0
Marcel Voigt <mv@noch.so>
2016-03-21 11:56:22 +0100

    feat: Add Folder methods delete and deleteContents

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,7 @@ Security   to invite users to upgrade in case of vulnerabilities.
 ### Added
 - New class `VcsVersionInfo` that wraps and enriches `VersionInfo` with the latest tag.
 - New class `OS` with methods `isWindows` and `hasBinary`.
+- New `Folder` methods `delete` and `deleteContents`
 
 ## [0.3.2] - 2016-03-16
 ### Added
--- src/Folder.php
+++ src/Folder.php
@@ -28,4 +28,64 @@ final class Folder
             error_get_last()['message']
         ));
     }
+
+    /**
+     * Delete a directory and all of its contents recursively.
+     *
+     * @param string $path Path of folder to delete
+     *
+     * @throws \RuntimeException If the folder does not exist or something could not be deleted.
+     */
+    public static function delete($path)
+    {
+        self::deleteContents($path);
+        if (@rmdir($path) === false) {
+            throw new \RuntimeException(sprintf(
+                    "Unable to delete folder '%s': %s",
+                    $path,
+                    error_get_last()['message']
+            ));
+        }
+    }
+
+    /**
+     * deleteContents of a folder recursively, but not the folder itself.
+     *
+     * @param string $path Path of folder whose contents will be deleted
+     *
+     * @throws \RuntimeException If the folder does not exist or something could not be deleted.
+     */
+    public static function deleteContents($path)
+    {
+        if (!is_dir($path)) {
+            throw new \RuntimeException(sprintf(
+                "Unable to delete folder '%s': folder must exist first",
+                $path
+            ));
+        }
+        /** @var \SplFileInfo[] $files */
+        $files = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($path, \RecursiveDirectoryIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::CHILD_FIRST
+        );
+        foreach ($files as $fileInfo) {
+            if ($fileInfo->isDir()) {
+                if (@rmdir($fileInfo->getRealPath()) === false) {
+                    throw new \RuntimeException(sprintf(
+                        "Unable to delete child folder '%s' in '%s': %s",
+                        $fileInfo->getRealPath(),
+                        $path,
+                        error_get_last()['message']
+                    ));
+                }
+            } elseif (@unlink($fileInfo->getRealPath()) === false) {
+                throw new \RuntimeException(sprintf(
+                    "Unable to delete file '%s' in folder '%s': %s",
+                    $fileInfo->getRealPath(),
+                    $path,
+                    error_get_last()['message']
+                ));
+            }
+        }
+    }
 }
