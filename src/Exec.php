b18065971214e8d50660c26fd7a4eafe31f68a82
Marcel Voigt <mv@noch.so>
2016-04-01 00:53:51 +0200

    fix: Make Exec tests pass

--- src/Exec.php
+++ src/Exec.php
@@ -74,13 +74,12 @@ class Exec
     public function getCommand(...$arguments)
     {
         $command = [];
-        foreach (array_merge($this->prefixes, $arguments) as $argument) {
-            $escapedArgument = escapeshellarg($argument);
-            // If nothing was escaped and there are no spaces, use the original argument
-            if (mb_substr($escapedArgument, 1, -1) === $argument && strpos($argument, ' ') === false) {
-                $escapedArgument = $argument;
-            }
-            $command[] = $escapedArgument;
+        $allArguments = array_merge($this->prefixes, $arguments);
+        if (count($allArguments) === 0) {
+            $allArguments[] = '';
+        }
+        foreach ($allArguments as $argument) {
+            $command[] = $this->escapeArgument($argument);
         }
         $commandString = implode(' ', $command);
         return $commandString;
@@ -128,5 +127,72 @@ class Exec
     public function __invoke(...$arguments)
     {
         return $this->run(...$arguments);
+    }
+
+    /**
+     * @param string $argument
+     *
+     * @return string
+     */
+    private function escapeArgument($argument)
+    {
+        // Always escape an empty argument so it doesn't get lost.
+        if ($argument === '') {
+            return escapeshellarg($argument);
+        }
+        if (!OS::isWindows()) {
+            return $this->escapeLinuxArgument($argument);
+        }
+        return $this->escapeWindowsArgument($argument);
+    }
+
+    /**
+     * @param string $argument
+     *
+     * @return string
+     */
+    private function escapeLinuxArgument($argument)
+    {
+        $escapedArgument = escapeshellarg($argument);
+        // If nothing had to be escaped and there are no spaces, use the original argument
+        if (mb_substr($escapedArgument, 1, -1) === $argument && strpos($argument, ' ') === false) {
+            return $argument;
+        }
+        return $escapedArgument;
+    }
+
+    /**
+     * @param string $argument
+     *
+     * @return string
+     */
+    private function escapeWindowsArgument($argument)
+    {
+        // Check if there's anything to escape
+        if (strpbrk($argument, " \t\n\v\"\\") === false) {
+            return $argument;
+        }
+        $escapedArgument = '"';
+        $strlen = mb_strlen($argument);
+        for ($i = 0; $i < $strlen; $i++) {
+            $backslashes = 0;
+            while ($i < $strlen && mb_substr($argument, $i, 1) === '\\') {
+                $i++;
+                $backslashes++;
+            }
+            if ($i === $strlen) {
+                // Escape all backslashes, but let the terminating double quote be interpreted as a meta character
+                $escapedArgument .= str_repeat('\\', $backslashes * 2);
+            } elseif (mb_substr($argument, $i, 1) === '"') {
+                // Escape all backslashes and the following quotation mark
+                $escapedArgument .= str_repeat('\\', $backslashes * 2 + 1);
+                $escapedArgument .= '"';
+            } else {
+                $escapedArgument .= str_repeat('\\', $backslashes);
+                $escapedArgument .= mb_substr($argument, $i, 1);
+            }
+        }
+        $escapedArgument .= '"';
+        return $escapedArgument;
     }
 }
